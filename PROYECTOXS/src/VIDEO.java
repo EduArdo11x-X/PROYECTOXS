7d63b0e9c6903e48dcc4b4a9b4ca4e5c811d81fc
EduArdo11x-X <kevin.cajeca.est@tecazuay.edu.ec>
2023-02-28 04:05:05 -0500

    Video de bienvenida

---
+++ PROYECTOXS/src/VIDEO.java
@@ -0,0 +1,18 @@
+
+package Clases;
+
+import Interfaces.IntroVideo;
+
+
+
+/**
+ *
+ * @author LENOVO
+ */
+public class VIDEO {
+    public static void main (String[] args){
+        IntroVideo repro = new IntroVideo();
+        repro.setVisible(true);
+    }
+    
+}
